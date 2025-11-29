97fa7a057c50ffa836930e0f2a2f7bed9c61429b
Dilara Özcan <ozcandilara05@gmail.com>
2025-11-29 09:06:37 +0300

    Create Faktoriyel.java

---
+++ loops/Faktoriyel.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz: ");
+        int sayi = input.nextInt();
+
+        int sonuc = 1;
+
+        for (int i = 1; i <= sayi; i++) {
+            sonuc *= i;
+        }
+        System.out.println("Faktöriyel sonucu: " + sonuc);
+    }
+}
