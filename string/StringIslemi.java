e0d147c3806c3b7d0530493def63ae5bbf194112
Dilara Özcan <ozcandilara05@gmail.com>
2025-11-29 09:31:35 +0300

    Create StringIslemi.java

---
+++ string/StringIslemi.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Bir metin yazınız: ");
+        String metin = input.nextLine();
+
+        System.out.println("Büyük harf " + metin.toUpperCase());
+        System.out.println("Küçük harf " + metin.toLowerCase());
+
+        char ilk = metin.charAt(0);
+        char son = metin.charAt(metin.length() - 1);
+
+        System.out.println("İlk karakter: " + ilk);
+        System.out.println("Son karakter: " + son);
+
+        String ters = "";
+
+        for (int i = metin.length() - 1; i >= 0; i--) {
+            ters += metin.charAt(i);
+        }
+        System.out.println("Ters metin: " + ters);
+    }
+}
